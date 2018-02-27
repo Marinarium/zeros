625d91002a44d830b115f763fcc90df430d134b7
Marina <schwmk@gmail.com>
2018-02-27 14:58:58 +0300

    improvement
    add a cycle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 module.exports = function getZerosCount(number) {
   var i = 0;
-  i = Math.floor(number/5) + Math.floor(number/Math.pow(5,2)) + Math.floor(number/Math.pow(5,3)) + 
-  Math.floor(number/Math.pow(5,4)) + Math.floor(number/Math.pow(5,5)) + Math.floor(number/Math.pow(5,6)) + 
-  Math.floor(number/Math.pow(5,7)) + Math.floor(number/Math.pow(5,8)) + Math.floor(number/Math.pow(5,9)) + 
-  Math.floor(number/Math.pow(5,10)) + Math.floor(number/Math.pow(5,11)) + Math.floor(number/Math.pow(5,12)) +
-  Math.floor(number/Math.pow(5,13));
+  while (number > 0) {
+    number = Math.floor(number / 5);
+    i = i + number;
+  }
   return i;
 }
