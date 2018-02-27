924a165d8970c60a61cbcaa196440d0c93c8ae50
Marina <schwmk@gmail.com>
2018-02-27 13:46:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var i = 0;
+  i = Math.floor(number/5) + Math.floor(number/Math.pow(5,2)) + Math.floor(number/Math.pow(5,3)) + 
+  Math.floor(number/Math.pow(5,4)) + Math.floor(number/Math.pow(5,5)) + Math.floor(number/Math.pow(5,6)) + 
+  Math.floor(number/Math.pow(5,7)) + Math.floor(number/Math.pow(5,8)) + Math.floor(number/Math.pow(5,9)) + 
+  Math.floor(number/Math.pow(5,10)) + Math.floor(number/Math.pow(5,11)) + Math.floor(number/Math.pow(5,12)) +
+  Math.floor(number/Math.pow(5,13));
+  return i;
 }
